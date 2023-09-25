15b14c8767cd218a3ff92cace28d69535e79d3d8
saurabh8753 <130946713+saurabh8753@users.noreply.github.com>
2023-09-25 14:53:10 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/VJBots/VJ-FILTER-BOT.git /VJ-FILTER-BOT 
+  git clone https://github.com/Saurabh8753/VJ-FILTER-BOT.git /VJ-FILTER-BOT 
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /VJ-FILTER-BOT 
